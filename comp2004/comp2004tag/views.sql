1f8da16d0060a1e8564cc3ae5335f0dcf1ab879c
Thomas Grainger <tom@yplanapp.com>
2015-09-06 17:25:45 +0100

    simplify

--- comp2004/comp2004tag/views.sql
+++
@@ -1,3 +0,0 @@
-CREATE VIEW currentstudents AS SELECT * FROM ds_students WHERE status = 'current';
-CREATE VIEW suspendedstudents AS SELECT * FROM ds_students WHERE status = 'suspended';
-CREATE VIEW leftstudents AS SELECT * FROM ds_students WHERE status = 'left';
